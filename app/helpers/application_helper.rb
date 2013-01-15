d61eccd1fb0805d3ce82d7625bf08802b7b8d25e
dodgerogers@hotmail.com <andy.rogers@teebox.co>
2013-01-15 23:59:55 +0800

    html5 video player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   
+  def html5_video(id)
+    
+    "<iframe class='youtube-player' type='text/html' width='640' height='385' src='http://www.youtube.com/embed/#{id}' frameborder='0'></iframe>"
+  end
 end
+
